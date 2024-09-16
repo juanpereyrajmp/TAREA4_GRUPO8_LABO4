7c90fb1f71ddef25a91145f67a24391070105dd7
juanpereyrajmp <juanpereyra.jmp@gmail.com>
2024-09-16 12:52:19 -0300

    Actualizacion de version del proyecto con el Ejercicio 1 completo

--- src/Ejercicio/Ejercicio1.java
+++ src/Ejercicio/Ejercicio1.java
@@ -1,6 +1,5 @@
 package Ejercicio;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -10,20 +9,25 @@ import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
 
 public class Ejercicio1 extends JFrame {
 
+
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField txtNombre;
 	private JTextField txtApellido;
 	private JTextField txtTelefono;
 	private JTextField txtFechaNac;
 
-	/**
-	 * Launch the application.
-	 */
+	private JButton btnMostrar;
+	private JLabel lblLosDatos;
+
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -37,11 +41,10 @@ public class Ejercicio1 extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
+
 	public Ejercicio1() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Contactos");
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);//DISPOSE_ON_CLOSE Cierra solo esta ventana
 		setBounds(100, 100, 600, 400);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -65,54 +68,135 @@ public class Ejercicio1 extends JFrame {
 		contentPane.add(lblFechaNac);
 		
 		txtNombre = new JTextField();
+		txtNombre.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent arg0) {
+				
+				char c = arg0.getKeyChar();
+				if((c!=' ')&& ((!Character.isLetter(c)))){
+					arg0.consume();
+				}
+			}
+		});
+		
+		
 		txtNombre.setBounds(227, 48, 150, 20);
 		contentPane.add(txtNombre);
 		txtNombre.setColumns(10);
 		
 		txtApellido = new JTextField();
+		txtApellido.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				char c = e.getKeyChar();
+				if((c!=' ')&& ((!Character.isLetter(c)))){
+					e.consume();
+				}
+				
+			}
+		});
 		txtApellido.setColumns(10);
 		txtApellido.setBounds(227, 83, 150, 20);
 		contentPane.add(txtApellido);
 		
 		txtTelefono = new JTextField();
+		txtTelefono.addKeyListener(new KeyAdapter() {
+			int cont = 0; //Contador para que solo se pueda ingresar una vez el "+" en el textfield numero
+			@Override
+			public void keyTyped(KeyEvent e) {
+				char c = e.getKeyChar();
+				if (c== '+') { 
+					if(cont ==0) {
+						cont++;
+					}
+					else {
+						e.consume();
+					}
+				}
+				if((c<'0' || c>'9')&& (c!='+') && (c!=' ')) {
+					e.consume();
+				}
+				
+			}
+		});
 		txtTelefono.setColumns(10);
 		txtTelefono.setBounds(227, 124, 150, 20);
 		contentPane.add(txtTelefono);
 		
 		txtFechaNac = new JTextField();
+		txtFechaNac.addKeyListener(new KeyAdapter() {
+			int contBarra = 0;
+			@Override
+			public void keyTyped(KeyEvent e) {
+				char c = e.getKeyChar();
+				 if (c == '/') {
+			            if (contBarra < 2) {
+			                contBarra++;
+			            } else {
+			                e.consume();
+			            }
+			        } else if ((c < '0' || c > '9')) {
+			            e.consume();
+			        }
+				 
+			}
+		});
 		txtFechaNac.setColumns(10);
 		txtFechaNac.setBounds(227, 155, 150, 20);
 		contentPane.add(txtFechaNac);
 		
-		JButton btnMostrar = new JButton("Mostrar");
-		
-		
-		
-		btnMostrar.setBounds(293, 198, 89, 23);
-		contentPane.add(btnMostrar);
-		
-		JLabel lblDatos = new JLabel("Los datos ingresados fueron:");
-		lblDatos.setBounds(87, 232, 331, 27);
-		contentPane.add(lblDatos);
-		
-		JPanel panel = new JPanel();
-		panel.setBackground(new Color(240, 255, 240));
-		panel.setBounds(87, 264, 431, 86);
-		contentPane.add(panel);
-		
-		JLabel lblMostrarDatos = new JLabel("");
-		panel.add(lblMostrarDatos);
-		lblMostrarDatos.setBackground(new Color(240, 255, 255));
-		//BOTON QUE MUESTRA CONTENIDO DE LAS TXT Y VALIDA LOS MISMOS.
-
-		btnMostrar.addActionListener(new ActionListener() {
+		 btnMostrar = new JButton("Mostrar");
+		 btnMostrar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//validar estado de las txt y set back color.
 				
-				String input = "Nombre : " + txtNombre.getText() + "   Apellido " + txtApellido.getText() + "  Telefono:" + txtTelefono.getText()+ "  Nacimiento : " + txtFechaNac.getText();
-				lblMostrarDatos.setText(input); 
+				if(txtNombre.getText().isEmpty()) {
+					txtNombre.setBackground(Color.RED);
+					lblLosDatos.setText("");
+				}
+				else {
+					txtNombre.setBackground(Color.WHITE);
+				}
+				if(txtApellido.getText().isEmpty()) {
+					txtApellido.setBackground(Color.RED);
+					lblLosDatos.setText("");
+				}
+				else {
+					txtApellido.setBackground(Color.WHITE);
+				}
+				if(txtTelefono.getText().isEmpty()) {
+					txtTelefono.setBackground(Color.RED);
+					lblLosDatos.setText("");
+				}
+				else {
+					txtTelefono.setBackground(Color.WHITE);
+				}
+				if(txtFechaNac.getText().isEmpty()) {
+					txtFechaNac.setBackground(Color.RED);
+					lblLosDatos.setText("");
+				}
+				else {
+					txtFechaNac.setBackground(Color.WHITE);
+				}
+				if(!txtNombre.getText().isEmpty() && !txtApellido.getText().isEmpty() && !txtTelefono.getText().isEmpty() && !txtFechaNac.getText().isEmpty()) {
+					lblLosDatos.setText("Los datos fueron: "+txtNombre.getText()+" "+txtApellido.getText()+" "+txtTelefono.getText()+" "+txtFechaNac.getText());
+					txtNombre.setText("");
+					txtApellido.setText("");
+					txtTelefono.setText("");
+					txtFechaNac.setText("");
+					txtNombre.setBackground(Color.WHITE);
+					txtApellido.setBackground(Color.WHITE);
+					txtTelefono.setBackground(Color.WHITE);
+					txtFechaNac.setBackground(Color.WHITE);
+				}
+				
 			}
 		});
+		btnMostrar.setBounds(241, 205, 89, 23);
+		contentPane.add(btnMostrar);
+		
+		lblLosDatos = new JLabel("");
+		lblLosDatos.setBounds(60, 252, 411, 14);
+		contentPane.add(lblLosDatos);
 		
 	}
 }
